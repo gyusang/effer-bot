9f43c7bf31b42b26272b558d17c7d954b229d9cf
Sanggyu Lee <efferlee@icloud.com>
2017-10-02 11:32:12 +0900

    Init

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/',function(req,res){
+	res.send('Hello World')
+})
+
+app.get('/webhook', function(req,res){
+	if(req.query['hub.verify_token'] ===
+	('gyusang'){
+		res.send(req.query['hub.challenge'])
+	})
+	res.send('No entry')
+})
+
+app.listen(app.get('port'), function(){
+	console.log('running on port', app.get('port'))
+})
