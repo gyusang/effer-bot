4aed3742386d38d5b2b458c8ce39b06d2f15ddad
Programming Account 2 <programming@MacBook-Pro.local>
2017-11-21 21:02:44 +0900

    Add terms

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const path = require('path');
 var messengerButton = "<html><head><title>EFFER Bot</title></head><body><h1>Facebook Messenger Bot</h1>This is a bot based on Facebook Messenger. To use the bot, message <a href=\"https://m.me/effer.bot\">me</a>.</div></body></html>";
-
+var term = "<!DOCTYPE html> <html>  <head> 	<title>Terms</title> 	<link rel='shortcut icon' href='http://www.iconeasy.com/icon/ico/Holiday/Halloween%201/jason.ico' /> 	<meta http-equiv=Content-Type content='text/html; charset=utf-8'> </head>  <body bgcolor=white>  <p style='font-size:200%;'>Terms of Service ('Terms')</p>   Last updated: 2017.11.21.<br/> <hr/> <p>Please read these Terms of Service ('Terms', 'Terms of Service') carefully before using the website operated by Sanggyu Co. ('us', 'we', or 'our').<br/>   Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.   By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service. </p> <b>Termination</b> <p> We may terminate or suspend access to our Service immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms.   All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability. </p> <b>Links To Other Web Sites</b> <p> Our Service may contain links to third-party web sites or services that are not owned or controlled by Sanggyu Co.   Sanggyu Co. has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that Sanggyu Co. shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services. </p> <b>Changes</b> <p> We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will try to provide at least 30 days' notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion. </p> <b>Contact Us</b><br/>   If you have any questions about these Terms, please contact us.  </body>  </html> "
 // The rest of the code implements the routes for our Express server.
 let app = express();
 
@@ -35,6 +35,19 @@ app.get('/', function(req, res) {
   res.end();
 });
 
+/*TODO thisthis*/
+app.get('/terms', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(terms);
+  res.end();
+});
+/**TODO*/
+app.post('/register', function (req, res) {
+  console.log(req.body);
+    res.sendStatus(200);
+  }
+});
+/**TODO**/
 // Message processing
 app.post('/webhook', function (req, res) {
   console.log(req.body);
